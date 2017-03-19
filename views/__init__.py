b6b9cb55c75b87aa190b6645e495b728e0c7ea42
Pavel <pavel@klinskih.com>
2017-03-19 17:42:25 +0500

    Залил шаблон индекс, выводящий всякую фигню

--- views/__init__.py
+++ views/__init__.py
@@ -5,8 +5,23 @@ from .auth import login
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
-    return 'Привет мир!'
+@login_required
+def index():
+    user = g.user
+    posts = [
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
 
 
 if __name__ == '__main__':
