0ca295a3b7ab279670266a0e49df2d6c2e8c3e50
Pavel <pavel@klinskih.com>
2017-03-19 17:09:31 +0500

    поменял авторизацию, теперь это flask login и openid

--- __init__.py
+++ __init__.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 from VetLife import views, models
+
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- views/auth.py
+++ views/auth.py
@@ -1,15 +1,20 @@
-from VetLife import app
-from VetLife.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from VetLife import app, db, lm, oid
+from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+  if g.user is not None and g.user.is_authenticated():
+      return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+      session['remember_me'] = form.remember_me.data
+      return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+  return render_template('login.html',
+      title = 'Sign In',
+      form = form,
+      providers = app.config['OPENID_PROVIDERS'])
