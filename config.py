8dbf0b7566d7d51e27020f4b1bfe6f64e7f01ca3
Klinskih <klinskih@gmail.com>
2017-03-19 11:54:57 +0300

    Не создавалась бд

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = '123'
 basedir = os.path.abspath(os.path.dirname(__file__))
 params = dict(login='kerlaeda_vetlife', psw='p5rNIfl3'
-              , db_ip='92.53.123.96', db_name='kerlaeda_vetlife')
+              , db_ip='localhost', db_name='kerlaeda_vetlife')
 
 SQLALCHEMY_DATABASE_URI = 'mysql://{login}:{psw}@{db_ip}/{db_name}'.format(**params)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
