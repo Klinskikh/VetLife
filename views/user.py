4198964faa4cdf1da7a66b9515bac399f3d25ab4
Pavel <pavel@klinskih.com>
2017-03-19 20:37:32 +0500

    Добавил темплейт для личного кабинета, сделал отдельный файл для обработки личного кабинета пользователя. В основном представлении сделал отображение перехода в личный кабинет для авторизованного пользователя.

---
+++ views/user.py
@@ -0,0 +1,19 @@
+from VetLife import app
+from flask_login import login_required
+from flask import flash, render_template, redirect, url_for
+from VetLife.models import User
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
