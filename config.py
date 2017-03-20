367b0f6689db202fff7b5555a9fcf2f0c48961cf
Anton Klinskih <a.klinskih@carbonsoft.ru>
2017-03-20 15:21:31 +0500

    Скрыты персональные данные

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+import yaml
+with open('config.yaml', 'r') as f:
+    config = yaml.load(f)
+
 WTF_CSRF_ENABLED = True
-SECRET_KEY = '123'
+SECRET_KEY = config['secret_key']
 
 
 OPENID_PROVIDERS = [
@@ -10,11 +14,7 @@ OPENID_PROVIDERS = [
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-params = dict(login='kerlaeda_vetlife', psw='p5rNIfl3'
-              , db_ip='92.53.123.96', db_name='kerlaeda_vetlife')
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{login}:{psw}@{db_ip}/{db_name}'.format(**params)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{db_login}:{db_psw}@{db_ip}/{db_name}'.format(**config)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
