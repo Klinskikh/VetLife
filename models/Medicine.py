cd84cf0d9e5c9b6d8c4b2bf263eee6d58bd3fc74
Anton Klinskih <a.klinskih@carbonsoft.ru>
2017-03-24 09:22:25 +0500

    Расширено поле описания препарата и добавлен вывод ошибок

--- models/Medicine.py
+++ models/Medicine.py
@@ -5,7 +5,7 @@ from VetLife import db
 class Medicine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, unique=True, info={'label': u'Наименование'})
-    descr = db.Column(db.String(1024), info={'label': u'Описание'})
+    descr = db.Column(db.String(65000), info={'label': u'Описание'})
 
     def __repr__(self):
         return self.title
