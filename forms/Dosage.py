78e0a922e693ecd38ef6ec301974ed354e0166ef
Anton Klinskih <klinskih@gmail.com>
2017-03-27 11:25:43 +0500

    Добавлена возможность задавать количество действующего вещества

--- forms/Dosage.py
+++ forms/Dosage.py
@@ -8,7 +8,6 @@ from VetLife import models
 
 class DosageForm(ModelForm):
     dosage_type = QuerySelectField(query_factory=models.DosageType.query.all, label=u"Тип дозировки")
-    unit = QuerySelectField(query_factory=models.Unit.query.all, label=u"Еденица измерения")
 
     class Meta:
         model = models.Dosage
--- forms/Medicine.py
+++ forms/Medicine.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from wtforms.widgets import TextArea
+from wtforms_alchemy import QuerySelectField
 
 from . import ModelForm
-from VetLife.models import Medicine
+from VetLife import models
 
 
 class MedicineForm(ModelForm):
+    unit = QuerySelectField(query_factory=models.Unit.query.all, label=u"Еденица измерения")
     class Meta:
-        model = Medicine
+        model = models.Medicine
         field_args = {'descr': {'widget': TextArea()}}
 
