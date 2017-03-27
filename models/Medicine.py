45fde7f01e0bffa4ac07ec428fbcc9b8887d498e
Anton Klinskih <klinskih@gmail.com>
2017-03-27 22:20:18 +0500

    Отладка работы с активным веществом

--- models/Medicine.py
+++ models/Medicine.py
@@ -8,7 +8,7 @@ class Medicine(db.Model):
     descr = db.Column(db.String(65000), info={'label': u'Описание'})
     active_amount = db.Column(db.Float, info={'label': u'Количество действующего вещества (мг/мл)'})
     active_substance_id = db.Column(db.Integer, db.ForeignKey('active_substance.id'))
-    active_substance = db.relationship('AcitveSubstance', backref=db.backref('medicines', lazy='dynamic'), info={'label': u'Активное вещество'})
+    active_substance = db.relationship('ActiveSubstance', backref=db.backref('medicines', lazy='dynamic'), info={'label': u'Активное вещество'})
 
     def __repr__(self):
         return self.title
--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 from .User import User, ROLE_USER, ROLE_ADMIN
 from .Unit import Unit
-from .Medicine import Medicine
 from .Dosage import Dosage
 from .Audit import Audit
 from .ActiveSubstance import ActiveSubstance
+from .Medicine import Medicine
 from .AnimalType import AnimalType
 
