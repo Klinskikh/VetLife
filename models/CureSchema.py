b7e23b4b3fbb5ab4f32471143205931fc6f566b8
Anton Klinskih <klinskih@gmail.com>
2017-04-24 14:44:56 +0500

    В схемы лечения добавлено описание лечения

--- models/CureSchema.py
+++ models/CureSchema.py
@@ -5,7 +5,8 @@ from VetLife import db
 class CureSchema(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, unique=True, info={'label': u'Наименование'})
-    descr = db.Column(db.String(65000), info={'label': u'Описание'})
+    descr = db.Column(db.String(65000), info={'label': u'Диагностика'})
+    crue = db.Column(db.String(65000), info={'label': u'Лечение'})
 
     def __repr__(self):
         return self.title
