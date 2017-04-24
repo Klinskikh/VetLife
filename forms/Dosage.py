c7c27b3d953884587bfa1e5e970634c2ec97bd20
Anton Klinskih <klinskih@gmail.com>
2017-04-24 15:54:40 +0500

    Добавлена возможность указывать десятичную часть через запятую

--- forms/Dosage.py
+++ forms/Dosage.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from wtforms.validators import Optional, DataRequired
 from wtforms_alchemy import QuerySelectField
+
+from .form_fields import MyFloatField
 from .. import db
 
 from . import ModelForm
@@ -8,6 +11,8 @@ from VetLife import models
 
 class DosageForm(ModelForm):
     animal_type = QuerySelectField(query_factory=lambda: db.session.query(models.AnimalType), label=u"Тип животного")
+    value = MyFloatField(u'от', validators=[DataRequired()])
+    value_max = MyFloatField(u'до', validators=[DataRequired()])
 
     class Meta:
         model = models.Dosage
